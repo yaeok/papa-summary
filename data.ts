8ea58993004c7ef20a31e06a507f59e19fee58ec
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-01-23 17:10:01 +0900

    deployエラーの解消(2)

--- data.ts
+++ data.ts
@@ -7,7 +7,6 @@ import { TaskStatus } from './types/TaskStatus'
 // price: number
 // categories: Category[]
 // createdAt: Date
-// updatedAt: Date | null
 
 export const products: Product[] = [
   {
@@ -28,7 +27,6 @@ export const products: Product[] = [
       },
     ],
     createdAt: new Date(),
-    updatedAt: null,
   },
   {
     id: '2',
@@ -37,7 +35,6 @@ export const products: Product[] = [
     content: 'content2',
     categories: [],
     createdAt: new Date(),
-    updatedAt: null,
   },
   {
     id: '3',
@@ -45,100 +42,6 @@ export const products: Product[] = [
     price: 3000,
     content: 'content3',
     categories: [],
-    createdAt: new Date(),
-    updatedAt: null,
-  },
-]
-
-export const tasks: Task[] = [
-  {
-    id: '1',
-    title: 'Task 1',
-    content: 'content1content1content1content1content1content1content1content1',
-    startDate: new Date(new Date().setDate(new Date().getDate() - 3)),
-    endDate: null,
-    status: TaskStatus.DOING,
-    createdAt: new Date(),
-  },
-  {
-    id: '2',
-    title: 'Task 2',
-    content: 'content2',
-    startDate: new Date(new Date().setDate(new Date().getDate() + 3)),
-    endDate: null,
-    status: TaskStatus.DONE,
-    createdAt: new Date(),
-  },
-  {
-    id: '3',
-    title: 'Task 3',
-    content: 'content3',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.NOTSTARTED,
-    createdAt: new Date(),
-  },
-  {
-    id: '4',
-    title: 'Task 4',
-    content: 'content4',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.NOTSTARTED,
-    createdAt: new Date(),
-  },
-  {
-    id: '5',
-    title: 'Task 5',
-    content: 'content5',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.DONE,
-    createdAt: new Date(),
-  },
-  {
-    id: '6',
-    title: 'Task 6',
-    content: 'content6',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.DOING,
-    createdAt: new Date(),
-  },
-  {
-    id: '7',
-    title: 'Task 7',
-    content: 'content7',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.NOTSTARTED,
-    createdAt: new Date(),
-  },
-  {
-    id: '8',
-    title: 'Task 8',
-    content: 'content8',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.NOTSTARTED,
-    createdAt: new Date(),
-  },
-  {
-    id: '9',
-    title: 'Task 9',
-    content: 'content9',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.DONE,
-    createdAt: new Date(),
-  },
-  {
-    id: '10',
-    title: 'Task 10',
-    content: 'content10',
-    startDate: new Date(),
-    endDate: null,
-    status: TaskStatus.DOING,
     createdAt: new Date(),
   },
 ]
