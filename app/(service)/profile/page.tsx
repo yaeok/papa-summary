9d0637b16e3b4038f59f00ce58dad39ffe8d258e
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-02-27 16:56:11 +0900

    deployエラーの解消(1)

--- app/(service)/profile/page.tsx
+++ app/(service)/profile/page.tsx
@@ -1,6 +1,3 @@
-import BabySection from './_components/baby_section/BabySection'
-import CategoriesSection from './_components/categories_section/CategoriesSection'
-import ProfileSection from './_components/profile_section/ProfileSection'
 import Section from './_components/section/Section'
 
 const Page = () => {
